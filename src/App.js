b1fdd1c20e7f17a9ae0e8d740e83b153af70b769
Suzan Ozkilic <suzandalgic@gmail.com>
2020-12-27 19:32:34 -0600

    login

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {BrowserRouter as Router ,Switch , Route} from "react-router-dom";
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
+import Home from "./home/Home";
+import Login from "./login/Login";
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+         <Route path="/login"><Login /></Route>
+         <Route path="/register">Register</Route>
+         <Route path="/" component={Home}></Route>
+        </Switch>
+        <Footer />
+     </div>
+    </Router>
+  );
+}
+
+export default App;
